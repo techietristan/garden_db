7498f3fd41996e378441e163fc7bb2b6e196c88e
Tristan Davis <techietristan@gmail.com>
2023-04-11 08:52:32 -0600

    fixed bug with handleEnter

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,21 @@ function App() {
   return (
     <div className="App">
       <h1>The Garden Database</h1>
+      <h2>{state.modeDescription}</h2> 
       <div className="card">
-        <button type='button' className='modeButton' onClick={() => updateState(({ mode: 'sproutSeeds', modeDescription: 'Sprouting Seeds'}))}>
-          Sprout Seeds
-        </button>
-        <button type='button' className='modeButton' onClick={() => updateState(({ mode: 'plantSeeds', modeDescription: 'Planting Seeds'}))}>
-          Plant Seeds
-        </button>
-        <button type='button' className='modeButton' onClick={() => updateState(({ mode: 'plantPlants', modeDescription: 'Planting Plants'}))}>
-          Plant Plants
-        </button>
-        <h2>{state.modeDescription}</h2> 
-          <form>
-            <AssetTagInputs state={state} />
-          </form>
-        </div>
+        <div className='buttons'>
+         <button type='button' className='modeButton' onClick={() => updateState(({ mode: 'sproutSeeds', modeDescription: 'Sprouting Seeds'}))}>
+            Sprout Seeds
+          </button>
+          <button type='button' className='modeButton' onClick={() => updateState(({ mode: 'plantSeeds', modeDescription: 'Planting Seeds'}))}>
+            Plant Seeds
+          </button>
+          <button type='button' className='modeButton' onClick={() => updateState(({ mode: 'plantPlants', modeDescription: 'Planting Plants'}))}>
+            Plant Plants
+          </button>
+        </div>  
+          <AssetTagInputs state={state} />
+      </div>
     </div>
   )
 }
--- src/components/AssetTagInputs.jsx
+++ src/components/AssetTagInputs.jsx
@@ -3,36 +3,43 @@ import { useState } from 'react'
 function AssetTagInputs(state) {
 
   const handleEnter = (event) => {
+    
+    event.preventDefault() 
     if (event.which == 13) {
-      event.preventDefault()
-      let inputClassName = event.target
-      if (inputClassName = 'input.seedAssetTag'){
+      const inputClassName = event.target
+      console.log(inputClassName)
+      if (inputClassName.classList.contains('seedAssetTag')){
         document.querySelectorAll('.podAssetTag')[0].focus()
+        
       }
     }
   }
 
+  const actionButtons = (
+  <div className='buttons'>
+    <button className='actionButton deleteInputs' title='Delete this Entry.'>&#10060;</button>
+    <button className='actionButton AddInputs' title='Add an Entry.'>&#10133;</button> 
+    <button className='actionButton updateDB' title='Update the Database.'>&#9989;</button>
+  </div>)
+
   const sproutSeeds = (    
       <div className='SproutSeeds'>
         <div className='inputField'>
-          <label htmlFor='seedAssetTag' autoFocus>Seed UPC: </label>
+          <label htmlFor='seedUPC' autoFocus>Seed UPC: </label>
           <input className='assetTag seedUPC' onKeyUp={event => handleEnter(event)}/>
-          <label htmlFor='seedAssetTag' autoFocus>Seed Name: </label>
-          <input className='assetTag seedName' onKeyUp={event => handleEnter(event)}/>
+          <label htmlFor='seedAssetTag' >Seed Asset Tag: </label>
+          <input className='assetTag seedAssetTag' onKeyUp={event => handleEnter(event)}/>
         </div>
         <div className='inputField'>
-          <label htmlFor='seedAssetTag' autoFocus>Seed Asset Tag: </label>
-          <input className='assetTag seedAssetTag' onKeyUp={event => handleEnter(event)}/>
+          <label htmlFor='seedName' >Seed Name: </label>
+          <input className='assetTag seedName' onKeyUp={event => handleEnter(event)}/>
           <label htmlFor='podAssetTag'>Pod Asset Tag: </label>
           <input className='assetTag podAssetTag' onKeyUp={event => handleEnter(event)}/>
         </div>
-        <div className='buttons'>
-          <button className='actionButton deleteInputs' title='Delete this Entry.'>&#10060;</button>
-          <button className='actionButton AddInputs' title='Add an Entry.'>&#10133;</button> 
-          <button className='actionButton updateDB' title='Update the Database.'>&#9989;</button>
-        </div>
+        {actionButtons}
       </div>
     )
+
 
   return sproutSeeds
 }
